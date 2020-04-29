d97a7ace21eaa4f648382d2bcefcb7d6ce2fbeeb
ashena <ashena@SOFT-PC>
2020-04-29 13:19:45 +0430

    store added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Todolists from './Components/Todolists'
-import store from './store/'
+import store from './store/index'
+import { observer } from 'mobx-react'
 
 function App() {
   return (
       <div>
+          {store.time.toLocaleTimeString()}
+          <button onClick={store.startClock}> Start</button>
+          <button onClick={store.stopClock}> Pause</button>
+          
           <Todolists />
        
       </div>
   );
 }
+
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,73 @@
-import {observable,action} from 'mobx'
+import React, { Component } from 'react';
+import { observable, computed, action, decorate } from "mobx";
+
+
+
+//class Store {
+
+//    time = new Date()
+
+//    startClock() {
+//        this.setInterval = this.setInterval(() => 
+//            this.time = new Date()
+//                , 1000)
+    
+//        }
+// }
+
+//decorate(Store, {
+//    time: observable
+//});
+
+//const store = new Store()
+//export default store
+
 class Store {
-    @observable time
+
+    @observable time = new Date();
 
     constructor() {
-        this.time = new Date()
+        this.interval = setInterval(() => { this.time = Date.now() }, 1000)
+    }
+   
+
+    startClock() {
+        this.interval = setInterval(() => { this.time = Date.now() },1000)
+        
+    }
+
+    pauseClock() {
+        clearInterval(this.interval)
     }
 }
+decorate(Store, {
+    time: observable,
+    startClock: action,
+    pauseClock: action
+});
 
 const store = new Store()
 export default store
+
+
+//class Timer {
+//    start = Date.now();
+//    current = Date.now();
+
+//    get elapsedTime() {
+//        return this.current - this.start + "milliseconds";
+//    }
+
+//    tick() {
+//        this.current = Date.now();
+//    }
+//}
+//decorate(Timer, {
+//    start: observable,
+//    current: observable,
+//    elapsedTime: computed,
+//    tick: action
+//});
+
+//const timer = new Timer()
+//export default timer
